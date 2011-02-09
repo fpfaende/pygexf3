8f413e725ca41dacabf701e6255c6684cee4d190
Fabien Pfaender <fabien.pfaender@me.com>
2011-02-09 03:35:41 -0800

    adapt to python3

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
-setup(name='pygexf',
+setup(name='pygexf3',
       version='0.2.2',
       packages=['gexf'],
-      url='http://github.com/paulgirard/pygexf',
-      author='Paul Girard',
-      author_email='paul.girard@sciences-po.fr'
+      url='http://github.com/pfpfaende/pygexf3',
+      author='Fabien Pfaender',
+      author_email='fabien@lueffyworld.net'
       )
